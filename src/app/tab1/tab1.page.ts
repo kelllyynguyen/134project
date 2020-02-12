5adbf962fc3d3aba3ecbe3bbf6d2cf079f13aa5a
kelllyynguyen <56004578+kelllyynguyen@users.noreply.github.com>
2020-02-11 17:48:33 -0800

    start of code - community page + 3 tabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  categories: any = [
+    {
+      "title": "Most Recent"
+    },
+    {
+      "title": "Self-Defense Tips"
+    },
+    {
+      "title": "Share a Story"
+    },
+    {
+      "title": "Reporting"
+    }
+  ];
 
-  constructor() {}
+  posts: any = [
+    {
+      "title": "Some Title",
+      "preview": "Text preview of some content",
+      "favorites": 25,
+      "comments": 20,
+    },
+    {
+      "title": "Some Title",
+      "preview": "Text preview of some content",
+      "favorites": 5,
+      "comments": 0,
+    },
+    {
+      "title": "Some Title",
+      "preview": "Text preview of some content",
+      "favorites": 15,
+      "comments": 2,
+    }
+  ]
+  constructor() {
+
+  }
 
 }
