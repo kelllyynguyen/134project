f7a02e1955625cb59b0684eeccb0617329bcdc1a
Joshua Kuan <jckuan1@uci.edu>
2020-02-25 15:21:12 -0800

    flic button framework

---
+++ Assure/Assure-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Assure-Bridging-Header1.h
+//  Assure
+//
+//  Created by Joshua Kuan on 2/25/20.
+//  Copyright © 2020 Joshua Kuan. All rights reserved.
+//
+
+#ifndef Assure_Bridging_Header1_h
+#define Assure_Bridging_Header1_h
+
+#import "simulator-wrapper/fliclibWrapper.h"
+
+#endif /* Assure_Bridging_Header1_h */
